ec335954162d43c0a82e544f4169c6652b87479a
Mihroban <mikkiusa11@gmail.com>
2019-01-30 15:56:13 -0500

    added new spec file

---
+++ specs/calculator.js
@@ -0,0 +1,22 @@
+
+
+describe('Protractor calculator demo test', function(){
+
+    it('locators should work',function(){
+        browser.get('https://juliemr.github.io/protractor-demo/');
+        element(by.model('first')).sendKeys("5");
+        element(by.model('second')).sendKeys("7");
+        element(by.id('gobutton')).click();
+
+        //Below will throw an erro, because it returns a promise,to get the
+        //specific text, we need to resolve that promise using then(function)
+        //console.log(element(by.css("h2[class='ng-biding']")).getText());
+
+        //In the below line of code, once the returned promise resolved, the result will be passed to argument'text' 
+        element(by.css("h2[class='ng-binding']")).getText().then(function(text){
+            console.log(text);
+        })
+
+
+    })
+});
---
+++ specs/spec.js
@@ -0,0 +1,20 @@
+
+describe('Enter name feature of AngularJS app',function(){
+    it('should enter name as Mikki',function(){
+
+        browser.get('https://angularjs.org/');
+        protractor.element(by.model('yourName')).sendKeys('Mikki');
+
+        var text = element(by.xpath('/html/body/div[2]/div[1]/div[2]/div[2]/div/h1'));
+        //expect(text.getText()).toEqual('Hello Mikki!XXX');
+        expect(text.getText()).toEqual('Hello Mikki!');
+    })
+});
+
+    describe('Verify the title ', function(){
+        it('Should get the title of the CRM Page', function(){
+            browser.get('http://www.protractortest.org');
+            expect(browser.getTitle()).toEqual('Protractor - end-to-end testing for AngularJS');
+        })
+
+}); 
